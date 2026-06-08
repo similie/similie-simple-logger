3ebcb24663f954ca2365632cae31026c73cc892a
Adam Smith <adam.smith@similie.org>
2026-06-08 12:17:36 +0900

    added capture resources

--- src/resources/devices/video-capture.h
+++ src/resources/devices/video-capture.h
@@ -23,7 +23,7 @@ class VideoCapture : public Device
 private:
     VideoCaptureStruct config;
     uint16_t eepromAddress = 0;
-    Relay relay = Relay(D7, false);
+    Relay relay = Relay(D3, false);
     Adafruit_VC0706 cam = Adafruit_VC0706(&Serial1);
     TCPClient client;
     Bootstrap *boots;
